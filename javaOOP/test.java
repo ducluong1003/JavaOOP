b0ea8507d63fdb000013a19d30893efb9f86fd92
Ducluong <anhductam123@gmail.com>
2024-01-11 23:36:23 +0700

    update main

--- javaOOP/test.java
+++ javaOOP/test.java
@@ -4,6 +4,7 @@ public class test {
  
         public static void main(String[] args) throws Exception {
             System.out.println("Hello, World!");
+            System.out.println("Hi");
         }
     }
 
