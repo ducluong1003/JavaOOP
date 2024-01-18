0ab8bb5e73159ce692f676bc43ceb9e8673790c4
Ducluong <anhductam123@gmail.com>
2024-01-18 21:28:13 +0700

    slide 92 chương 2

--- Lab2/src/ifelse.java
+++ Lab2/src/ifelse.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ifelse {
+public class IfElse {
     public static void main(String[] args) {
         System.out.print(" Nhap vao mot so : ");
         Scanner sc = new Scanner(System.in);
--- Lab2/src/toanTu.java
+++ Lab2/src/toanTu.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class toanTu {
+public class ToanTu {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Nhap vao so thu nhat : ");
