a7d111d4db57728c03209b1115891e2d03506229
Ducluong <anhductam123@gmail.com>
2024-05-22 16:28:32 +0700

    LAD320 _cau1

---
+++ BaiKiemTra/src/LAD320Person.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LAD320Person {
+    Scanner sc = new Scanner(System.in);
+
+    public int Id;
+    public String Ten;
+    public float CanNang ;
+    public void LAD320Person( int Id, String Name, float CanNang)
+    {
+        this.Id = Id;
+        this.Ten = Ten;
+        this.CanNang = CanNang;
+    }
+   
+   
+
+    
+}
